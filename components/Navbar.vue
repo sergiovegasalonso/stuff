48b57759f90bd9e3e062e473cce09d0e9b7968c9
sergio <sergio.vegas.alonso@gmail.com>
2022-03-03 21:28:19 +0100

    First commit.

---
+++ components/Navbar.vue
@@ -0,0 +1,5 @@
+<template>
+  <NuxtLink to="/">Home page</NuxtLink>
+  <NuxtLink to="/about">About page</NuxtLink>
+  <NuxtLink to="/portfolio">Portfolio page</NuxtLink>
+</template>
---
+++ pages/portfolio.vue
@@ -0,0 +1,1 @@
+<template>Portfolio</template>
