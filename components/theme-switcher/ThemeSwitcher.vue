bf23a65f394c4d950c540a856b91f78ff6dffc06
Sergio Vegas Alonso <svegas@bilbomatica.es>
2022-03-04 17:54:11 +0100

    Ok.

--- components/theme-switcher/ThemeSwitcher.vue
+++ components/theme-switcher/ThemeSwitcher.vue
@@ -12,5 +12,31 @@ const toggleTheme = (): void => {
 </script>
 
 <template>
-    <button @click="toggleTheme()"></button>
+    <div id="theme-switcher" class="wrapper" @click="toggleTheme()">
+    <div class="rail">
+        <div class="switch"></div>
+    </div>
+    </div>
 </template>
+
+<style scoped>
+.wrapper {
+    width: 2.25rem;
+    height: 1rem;
+}
+.rail {
+    width: 2.25rem;
+    height: 1rem;
+    background-color: red;
+    border-radius: 1rem;
+}
+
+.switch {
+    position: absolute;
+    height: 1.50rem;
+    width: 1.50rem;    
+    background: black;
+    border-radius: 100%;
+    top: -25%;
+}
+</style>
